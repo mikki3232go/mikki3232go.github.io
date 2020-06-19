d5125918f0cc01bccc8e7e8cbc5802ca585ae8c6
Sangkon Han <sigmadream@gmail.com>
2020-06-19 14:32:24 +0900

    init

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.header').height($(window).height())
+})
+
+// window.onresize = window.onload = function() {
+//     document.getElementsById('header').style.height = this.innerHeight + 'px'
+// }
